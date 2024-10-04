f326cbf639686d81b877eb425bba5ccba374d222
aryastywn <aryasetyaw7@gmail.com>
2024-10-04 09:50:57 +0800

    2209106101 M.Arya Setyawan

---
+++ pertemuan4/pert4/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:pert4/pages/detail_page.dart';
+import 'package:pert4/pages/home_page.dart';
+// import 'package:pert4/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter guaa tuhh',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
---
+++ pertemuan4/pert4/lib/widget/custom_text_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final String labelText;
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.labelText,
+    required this.hintText,
+  });
+
+  @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: const Color(0xFFFFD18E), // Mengatur warna latar belakang
+        hintText: widget.hintText,
+        labelText: widget.labelText,
+        labelStyle: GoogleFonts.poppins(
+            color: Colors.black, fontSize: 15, fontWeight: FontWeight.w500),
+        hintStyle: GoogleFonts.poppins(
+            color: Colors.black, fontSize: 15, fontWeight: FontWeight.w500),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+  }
+}
